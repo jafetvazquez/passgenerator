415f9b79ba57e507af923da5403449775e579cf3
Jafet Vazquez <jetvax.jv@gmail.com>
2024-10-25 17:58:07 -0600

    first commit

---
+++ src/app/passgen/passgen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passgen',
+  standalone: true,
+  imports: [],
+  templateUrl: './passgen.component.html',
+  styleUrl: './passgen.component.css'
+})
+export default class PassgenComponent {
+
+}
