65780e1e3da437f114888182731de12f36c9d587
Jafet Vazquez <jetvax.jv@gmail.com>
2024-11-13 16:34:04 -0600

    icon changed, and ui in smartphone are done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      'sm': '640px',    // Cambiamos el breakpoint sm a 480px
+      'sm': '340px',    // Cambiamos el breakpoint sm a 480px
       'md': '768px',    // md sigue siendo 768px
       'lg': '1024px',   // lg sigue siendo 1024px
       'xl': '1280px',   // xl sigue siendo 1280px
